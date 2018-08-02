2ea7f412dd1d64dbab2e0ef0e4754d650d56874d
Stephane Le Henanff <github@le-henanff.fr>
2018-08-02 13:56:03 +0200

    packages_deps : introduction of the include_rmd parameter

--- R/package.R
+++ R/package.R
@@ -1,13 +1,18 @@
 #' Get package dependancies.
 #'
+#' Find packages dependances declared into R scripts and Rmd files.
+#'
 #' @param package_path Package path.
+#' @param include_rmd If \code{TRUE} then find dependances into Rmd files as well.
 #'
 #' @return A dependencies vector.
 #'
 #' @export
-package_deps <- function(package_path = getwd()) {
+package_deps <- function(package_path = getwd(), include_rmd = TRUE) {
 
-  deps <- list.files(package_path, pattern = "\\.(R|Rmd)$", full.names = TRUE, recursive = TRUE) %>%
+  pattern <- ifelse(include_rmd, "\\.(R|Rmd)$", "\\.R$")
+
+  deps <- list.files(package_path, pattern = pattern, full.names = TRUE, recursive = TRUE) %>%
     lapply(readr::read_lines) %>%
     unlist() %>%
     stringr::str_match_all("([[:alnum:]\\.]+)::") %>%
